dcab97f5bd9a46f8893217462f8872454b74ce8e
Anirban Prottoy <anirbanprottoy0@gmail.com>
2025-01-04 21:25:06 +0600

    Add files via upload

---
+++ DataStructure/2nd infix-postfix/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include "Stack.h"
+#include "StackInt.h"
+using namespace std;
+int main()
+{
+    int size=100;
+    stack s(size);
+    //char infix[]="A+(B*C-(D/E^F)*G)*H";
+    char infix[]="";
+    cout<<"Input Infix Value: ";
+    cin>>infix;
+    
+    string postfix=s.InfixtoPostfix(infix,size);
+    cout<<"Postfix: "<<postfix<<endl;
+
+    return 0;
+}
